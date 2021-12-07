740e83fff4a95a8e8fb5922489f7bccb4402f5a0
Valdo Romao <v.romao@thecodingmachine.com>
2021-12-07 13:56:24 +0000

    Fixed special zones and exits

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,81 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+import {bootstrapExtra} from '@workadventure/scripting-api-extra'
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+console.log('Script started successfully');
 
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
+async function extendedFeatures() {
+    try {
+        await bootstrapExtra()
+        console.log('Scripting API Extra loaded successfully');
+    } catch (error) {
+        console.error('Scripting API Extra ERROR',error);
+    }
+}
+extendedFeatures();
 
-let hiddenBallsZone: string = "hiddenBallInterior";
+// Manage popups
+let currentLayer: string;
 let currentPopup: any;
 
 const hiddenBallsCodes = [
     {
-        zone: 'hiddenBallOutdoors',
+        zone: 'hiddenBallOutdoor',
         code: 'E2TV9H'
     },
     {
-        zone: 'hiddenBallInterior',
+        zone: 'hiddenBallReception',
         code: 'CUNB97'
     },
     {
-        zone: 'hiddenBallWorkshops',
+        zone: 'hiddenBallMeetings',
         code: '8J9VYC'
     },
     {
-        zone: 'hiddenBallSpeakeasy',
+        zone: 'hiddenBallBar',
         code: 'VDDFD3'
     },
 ]
 
-WA.room.onEnterZone('hiddenBallOutdoors', () => {
-    hiddenBallsZone = 'hiddenBallOutdoors'
-    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
-})
-WA.room.onEnterZone('hiddenBallInterior', () => {
-    hiddenBallsZone = 'hiddenBallInterior'
-    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
-})
-WA.room.onEnterZone('hiddenBallWorkshops', () => {
-    hiddenBallsZone = 'hiddenBallWorkshops'
-    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
-})
-WA.room.onEnterZone('hiddenBallSpeakeasy', () => {
-    hiddenBallsZone = 'hiddenBallSpeakeasy'
-    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
-})
+// Need Help / Follow Us
+WA.room.onEnterLayer('hiddenBallOutdoor').subscribe(() => openPopup('hiddenBallOutdoor'));
+WA.room.onLeaveLayer('hiddenBallOutdoor').subscribe(closePopup);
 
-WA.room.onLeaveZone(hiddenBallsZone, closePopup)
+WA.room.onEnterLayer('hiddenBallReception').subscribe(() => openPopup('hiddenBallReception'));
+WA.room.onLeaveLayer('hiddenBallReception').subscribe(closePopup);
 
-function openPopup(zoneName: string, popupName: string) {
-    const zone = hiddenBallsCodes.find((item) => {
-        return item.zone == zoneName
+WA.room.onEnterLayer('hiddenBallMeetings').subscribe(() => openPopup('hiddenBallMeetings'));
+WA.room.onLeaveLayer('hiddenBallMeetings').subscribe(closePopup);
+
+WA.room.onEnterLayer('hiddenBallBar').subscribe(() => openPopup('hiddenBallBar'));
+WA.room.onLeaveLayer('hiddenBallBar').subscribe(closePopup);
+
+// Popup management functions
+function openPopup(layerName: string) {
+    currentLayer = layerName
+    const popupName = layerName + 'Popup'
+    const popupConfig = hiddenBallsCodes.find((item) => {
+        return item.zone == layerName
     });
-    currentPopup = WA.openPopup(popupName, `Congratulations! You found a ball of the EURO 2020. Send the code below to the event organizer. Find the 4 hidden balls and you will get a unique reward! > CODE: ${zone?.code} <`, [
-        {
-            label: "Got it!",
-            className: "normal",
-            callback: (popup => {
-                popup.close()
-            })
-        }]
-    )
-}
 
-function closePopup() {
-    if (currentPopup !== undefined) {
-        currentPopup.close()
-        currentPopup = undefined
+    if (typeof popupConfig !== 'undefined') {
+        // @ts-ignore otherwise we can't use zone.cta object
+        currentPopup = WA.ui.openPopup(popupName,
+            `Congratulations!\n
+            You found a ball of the EURO 2020. Send the code below to the event organizer. Find the 4 hidden balls and you will get a unique reward!\n
+            CODE: ${popupConfig?.code}`, [
+            {
+                label: "Got it!",
+                className: "normal",
+                callback: (popup => {
+                    popup.close()
+                })
+            }]
+        )
+    }
+}
+function closePopup(){
+    if (typeof currentPopup !== 'undefined') {
+        currentPopup.close();
+        currentPopup = undefined;
     }
 }
