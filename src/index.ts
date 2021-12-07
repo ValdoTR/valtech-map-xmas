2347b8d356b54aa1b36b70ae36d4174c0a89c93d
ValdoTR <vteixeiraromao@gmail.com>
2021-12-07 10:56:49 +0000

    Deploying to gh-pages from master @ a925c7a90942b79e4f08351e41e844cfe964eefd 🚀

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,65 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
-let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
+let hiddenBallsZone: string = "hiddenBallInterior";
+let currentPopup: any;
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+const hiddenBallsCodes = [
+    {
+        zone: 'hiddenBallOutdoors',
+        code: 'E2TV9H'
+    },
+    {
+        zone: 'hiddenBallInterior',
+        code: 'CUNB97'
+    },
+    {
+        zone: 'hiddenBallWorkshops',
+        code: '8J9VYC'
+    },
+    {
+        zone: 'hiddenBallSpeakeasy',
+        code: 'VDDFD3'
+    },
+]
+
+WA.room.onEnterZone('hiddenBallOutdoors', () => {
+    hiddenBallsZone = 'hiddenBallOutdoors'
+    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
+})
+WA.room.onEnterZone('hiddenBallInterior', () => {
+    hiddenBallsZone = 'hiddenBallInterior'
+    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
+})
+WA.room.onEnterZone('hiddenBallWorkshops', () => {
+    hiddenBallsZone = 'hiddenBallWorkshops'
+    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
+})
+WA.room.onEnterZone('hiddenBallSpeakeasy', () => {
+    hiddenBallsZone = 'hiddenBallSpeakeasy'
+    openPopup(hiddenBallsZone, hiddenBallsZone + 'Popup')
 })
 
-WA.room.onLeaveZone('clock', closePopUp)
+WA.room.onLeaveZone(hiddenBallsZone, closePopup)
 
-function closePopUp(){
+function openPopup(zoneName: string, popupName: string) {
+    const zone = hiddenBallsCodes.find((item) => {
+        return item.zone == zoneName
+    });
+    currentPopup = WA.openPopup(popupName, `Congratulations! You found a ball of the EURO 2020. Send the code below to the event organizer. Find the 4 hidden balls and you will get a unique reward! > CODE: ${zone?.code} <`, [
+        {
+            label: "Got it!",
+            className: "normal",
+            callback: (popup => {
+                popup.close()
+            })
+        }]
+    )
+}
+
+function closePopup() {
     if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
+        currentPopup.close()
+        currentPopup = undefined
     }
 }
