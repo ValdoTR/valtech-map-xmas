decf50f9f09c31b8a5dbf2bbb4beeb4d1e9560c4
Valdo Romao <v.romao@thecodingmachine.com>
2021-12-07 23:42:43 +0000

    Added some xmas assets

--- src/index.ts
+++ src/index.ts
@@ -17,43 +17,43 @@ extendedFeatures();
 let currentLayer: string;
 let currentPopup: any;
 
-const hiddenBallsCodes = [
+const hiddenStonesCodes = [
     {
-        zone: 'hiddenBallOutdoor',
-        code: 'E2TV9H'
+        zone: 'hiddenStoneOutdoor',
+        code: 'P7DLX'
     },
     {
-        zone: 'hiddenBallReception',
-        code: 'CUNB97'
+        zone: 'hiddenStoneReception',
+        code: 'EG2E3'
     },
     {
-        zone: 'hiddenBallMeetings',
-        code: '8J9VYC'
+        zone: 'hiddenStoneMeetings',
+        code: 'W4YMJ'
     },
     {
-        zone: 'hiddenBallBar',
-        code: 'VDDFD3'
+        zone: 'hiddenStoneBar',
+        code: 'A4L9Q'
     },
 ]
 
 // Need Help / Follow Us
-WA.room.onEnterLayer('hiddenBallOutdoor').subscribe(() => openPopup('hiddenBallOutdoor'));
-WA.room.onLeaveLayer('hiddenBallOutdoor').subscribe(closePopup);
+WA.room.onEnterLayer('hiddenStoneOutdoor').subscribe(() => openPopup('hiddenStoneOutdoor'));
+WA.room.onLeaveLayer('hiddenStoneOutdoor').subscribe(closePopup);
 
-WA.room.onEnterLayer('hiddenBallReception').subscribe(() => openPopup('hiddenBallReception'));
-WA.room.onLeaveLayer('hiddenBallReception').subscribe(closePopup);
+WA.room.onEnterLayer('hiddenStoneReception').subscribe(() => openPopup('hiddenStoneReception'));
+WA.room.onLeaveLayer('hiddenStoneReception').subscribe(closePopup);
 
-WA.room.onEnterLayer('hiddenBallMeetings').subscribe(() => openPopup('hiddenBallMeetings'));
-WA.room.onLeaveLayer('hiddenBallMeetings').subscribe(closePopup);
+WA.room.onEnterLayer('hiddenStoneMeetings').subscribe(() => openPopup('hiddenStoneMeetings'));
+WA.room.onLeaveLayer('hiddenStoneMeetings').subscribe(closePopup);
 
-WA.room.onEnterLayer('hiddenBallBar').subscribe(() => openPopup('hiddenBallBar'));
-WA.room.onLeaveLayer('hiddenBallBar').subscribe(closePopup);
+WA.room.onEnterLayer('hiddenStoneBar').subscribe(() => openPopup('hiddenStoneBar'));
+WA.room.onLeaveLayer('hiddenStoneBar').subscribe(closePopup);
 
 // Popup management functions
 function openPopup(layerName: string) {
     currentLayer = layerName
     const popupName = layerName + 'Popup'
-    const popupConfig = hiddenBallsCodes.find((item) => {
+    const popupConfig = hiddenStonesCodes.find((item) => {
         return item.zone == layerName
     });
 
@@ -61,7 +61,7 @@ function openPopup(layerName: string) {
         // @ts-ignore otherwise we can't use zone.cta object
         currentPopup = WA.ui.openPopup(popupName,
             `Congratulations!\n
-            You found a ball of the EURO 2020. Send the code below to the event organizer. Find the 4 hidden balls and you will get a unique reward!\n
+            You found a Magic Stone. Send the code below to the event organizer. Find the 4 hidden Stones and you will get a unique reward!\n
             CODE: ${popupConfig?.code}`, [
             {
                 label: "Got it!",
